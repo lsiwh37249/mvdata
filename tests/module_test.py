d68ec5aa0532ce8ee6ef44eb9032e9112baba022
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-14 19:30:44 +0900

    test 추가

--- tests/module_test.py
+++ tests/module_test.py
@@ -7,5 +7,11 @@ from mvdata.ml import save_json,req,save_movies
 #save_movices : year
 def test_save_movies():
     r = save_movies(year=2015, sleep_time=0.1)
+    r = save_movies(year=2016, sleep_time=0.1)
+    r = save_movies(year=2017, sleep_time=0.1)
+    r = save_movies(year=2018, sleep_time=0.1)
+    r = save_movies(year=2019, sleep_time=0.1)
+    r = save_movies(year=2020, sleep_time=0.1)
+    r = save_movies(year=2021, sleep_time=0.1)
     assert r
 
